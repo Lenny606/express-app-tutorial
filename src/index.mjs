54abe4f92f7db606501f3fe695e513f8c764ce99
Tomas Kravcik <kravcik@procorp.cz>
2025-01-26 18:17:40 +0100

    setup app

---
+++ src/index.mjs
@@ -0,0 +1,18 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
