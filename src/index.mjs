8901be9a1a47b179db837d872da90b749b1c87ca
Tomas Kravcik <kravcik@procorp.cz>
2025-01-26 18:49:30 +0100

    post

--- src/index.mjs
+++ src/index.mjs
@@ -1,6 +1,7 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json()); //native parser
 const PORT = process.env.PORT || 3000;
 
 
@@ -10,16 +11,24 @@ app.get('/api/users/:id?filter=&value=', (req, res) => {
     const parsedId = parseInt(req.params.id)
     const {query: {filter, value}} = req
 
-//query params validation + filter logic
+    //query params validation + filter logic
 
 
     if (isNaN(parsedId)) {
         return res.status(400).send({msg: "Invalid id"})
     }
-//find resource
-    response.status(200).send({msg: "test", param: parsedId})
+    //find resource
+
+    return response.status(200).send({msg: "test", param: parsedId})
 })
 
+app.post('/api/users', (req, res) => {
+
+    const {body} = req
+    //logic
+    const resource = null
+    return response.status(201).send({msg: "test", data: resource})
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
