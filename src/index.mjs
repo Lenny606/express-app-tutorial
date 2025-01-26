8b1f9438d01b6f64ac31713c8fed0af4d9b15441
Tomas Kravcik <kravcik@procorp.cz>
2025-01-26 18:28:18 +0100

    get

--- src/index.mjs
+++ src/index.mjs
@@ -4,13 +4,21 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 
+//GET
+app.get('/api/users/:id?filter=&value=', (req, res) => {
+
+    const parsedId = parseInt(req.params.id)
+    const {query: {filter, value}} = req
+
+//query params validation + filter logic
 
 
-
-
-
-
-
+    if (isNaN(parsedId)) {
+        return res.status(400).send({msg: "Invalid id"})
+    }
+//find resource
+    response.status(200).send({msg: "test", param: parsedId})
+})
 
 
 app.listen(PORT, () => {
