82232aff0261500d0ca833530240cd1828f526f4
Tomas Kravcik <kravcik@procorp.cz>
2025-03-01 19:27:44 +0100

    router

---
+++ routes/users.mjs
@@ -0,0 +1,45 @@
+import {Router} from 'express'
+import {query, validationResult, body, matchedData, checkSchema} from 'express-validator'
+import {createUserValidation} from "../schemaValidator/schemaValidator.js";
+
+const usersRouter = Router()
+
+
+// GET /users
+usersRouter.post("/api/users", checkSchema(createUserValidation),
+    (req, res) => {
+        const {body} = req
+        //logic
+
+        //validation
+        const result = validationResult(req)
+        if (!result.isEmpty()) {
+            return res.status(400).json({errors: result.array()})
+        }
+        const data = matchedData(req)
+
+        const resource = {}
+        return response.status(201).send({msg: "test", data: resource})
+    })
+usersRouter.get('/api/users/:id?filter=&value=',
+    query('filter').isString().notEmpty().isLength({min: 3}).withMessage("Filter validation"),
+    (req, res) => {
+
+        const parsedId = parseInt(req.params.id)
+        const {query: {filter, value}} = req
+
+        //query params validation + filter logic
+        //validator is attached in req
+        const result = validationResult(req)
+        if (!result.isEmpty()) {
+            return res.status(400).json({errors: result.array()})
+        }
+        if (isNaN(parsedId)) {
+            return res.status(400).send({msg: "Invalid id"})
+        }
+        //find resource
+
+        return response.status(200).send({msg: "test", param: parsedId})
+    })
+
+export default usersRouter
--- src/index.mjs
+++ src/index.mjs
@@ -1,6 +1,7 @@
 import express from 'express';
 import {query, validationResult, body, matchedData, checkSchema} from 'express-validator'
 import {createUserValidation} from "../schemaValidator/schemaValidator.js";
+import usersRouter from "../routes/users.mjs"
 
 const app = express();
 app.use(express.json()); //native parser
@@ -11,46 +12,8 @@ const loggingMiddleware = (req, res, next) => {
     console.log("Request received");
     next();
 }
+app.use(usersRouter)
 
-
-//GET
-app.get('/api/users/:id?filter=&value=',
-    query('filter').isString().notEmpty().isLength({min: 3}).withMessage("Filter validation"),
-    (req, res) => {
-
-        const parsedId = parseInt(req.params.id)
-        const {query: {filter, value}} = req
-
-        //query params validation + filter logic
-        //validator is attached in req
-        const result = validationResult(req)
-        if (!result.isEmpty()) {
-            return res.status(400).json({errors: result.array()})
-        }
-        if (isNaN(parsedId)) {
-            return res.status(400).send({msg: "Invalid id"})
-        }
-        //find resource
-
-        return response.status(200).send({msg: "test", param: parsedId})
-    })
-
-app.post(
-    '/api/users', checkSchema(createUserValidation),
-    (req, res) => {
-        const {body} = req
-        //logic
-
-        //validation
-        const result = validationResult(req)
-        if (!result.isEmpty()) {
-            return res.status(400).json({errors: result.array()})
-        }
-        const data = matchedData(req)
-
-        const resource = {}
-        return response.status(201).send({msg: "test", data: resource})
-    })
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
